e1d398a739054e7e2b81051f213020f061a9f205
rsmith155 <Raj@Rogers-MacBook-Pro.local>
2019-04-30 01:55:06 -0400

    commented out changes

--- js/main.js
+++ js/main.js
@@ -174,7 +174,7 @@ PlayState.init = function () {
     }, this);
 
     this.coinPickupCount = -100;
-    
+    //this.sfx.money.play();
 };
 
 PlayState.preload = function () {
@@ -199,7 +199,7 @@ PlayState.preload = function () {
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
     this.game.load.audio('sfx:coin', 'audio/coin.wav');
     this.game.load.audio('sfx:stomp', 'audio/stomp.wav');
-    this.game.load.audio('sfx:money', 'audio/money.mp3');
+    //this.game.load.audio('sfx:money', 'audio/money.mp3');
 
 
 };
@@ -210,7 +210,7 @@ PlayState.create = function () {
         jump: this.game.add.audio('sfx:jump'),
         coin: this.game.add.audio('sfx:coin'),
         stomp: this.game.add.audio('sfx:stomp')
-        money: this.game.add.audio('sfx:money')
+        //money: this.game.add.audio('sfx:money')
     };
 
     // create level
